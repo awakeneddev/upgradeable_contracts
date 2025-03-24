460c5c99b46f2fbe20d50aee471b9eed4002a022
awakeneddev <awakeneddevs@gmail.com>
2025-03-24 22:41:22 +0545

    ENV SETUP

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,16 @@ require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_KEY=process.env.ALCHEMY_KEY;
+const PRIVATE_KEY=process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.28",
   defaultNetwork: "hardhat",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/5sp--_xRAQRILrrILaWf31A_DdKM4ypW",
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
       accounts: [
-        "0xeeea76b57ca6a68dd248b03e92ff1200c15f9145560d369a95428cd9d4c37a13",
+        PRIVATE_KEY,
       ],
     },
   },
