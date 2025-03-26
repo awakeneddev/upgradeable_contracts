702a950b2ac440c74eb15fafd396d60b55c26f83
awakeneddev <awakeneddevs@gmail.com>
2025-03-26 16:23:16 +0545

    Private Key Updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,18 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 
 require("@openzeppelin/hardhat-upgrades");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const ALCHEMY_KEY=process.env.ALCHEMY_KEY;
-const PRIVATE_KEY=process.env.PRIVATE_KEY;
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.28",
   defaultNetwork: "hardhat",
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
-      accounts: [
-        PRIVATE_KEY,
-      ],
+      accounts: [`${PRIVATE_KEY}`],
     },
   },
 };
